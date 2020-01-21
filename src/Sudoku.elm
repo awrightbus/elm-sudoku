a4d4b7157ce0296e4a9d96949166f4c6789449b4
Christopher Miller <miller1997@gmail.com>
2020-01-21 16:34:50 -0500

    Added check row,col,block functions

--- src/Sudoku.elm
+++ src/Sudoku.elm
@@ -13,6 +13,11 @@ type Value
     | Nine
 
 
+values : List (Maybe Value)
+values =
+    [ Just One, Just Two, Just Three, Just Four, Just Five, Just Six, Just Seven, Just Eight, Just Nine ]
+
+
 type alias Pos =
     { x : Int
     , y : Int
@@ -20,8 +25,70 @@ type alias Pos =
 
 
 type alias Cell =
-    ( Pos, Value )
+    ( Pos, Maybe Value )
 
 
 type alias Board =
     List Cell
+
+
+findPossibilities : Cell -> Board -> List (Maybe Value)
+findPossibilities c b =
+    let
+        row =
+            inRow c b
+
+        col =
+            inCol c b
+
+        block =
+            inBlock c b
+    in
+    values
+        |> filterOut row
+        |> filterOut col
+        |> filterOut block
+
+
+filterOut : (a -> Bool) -> List a -> List a
+filterOut f xs =
+    List.filter (not << f) xs
+
+
+inRow : Cell -> Board -> Maybe Value -> Bool
+inRow ( pos, _ ) b v =
+    let
+        row =
+            List.filter (\( p, _ ) -> p.x == pos.x) b
+
+        rowValues =
+            List.map (\( _, val ) -> val) row
+    in
+    List.member v rowValues
+
+
+inCol : Cell -> Board -> Maybe Value -> Bool
+inCol ( pos, _ ) b v =
+    let
+        col =
+            List.filter (\( p, _ ) -> p.y == pos.y) b
+
+        colValues =
+            List.map (\( _, val ) -> val) col
+    in
+    List.member v colValues
+
+
+inBlock : Cell -> Board -> Maybe Value -> Bool
+inBlock ( pos, _ ) b v =
+    let
+        getBlock =
+            \x -> (x - 1) // 3
+
+        block =
+            List.filter (\( p, _ ) -> getBlock p.x == getBlock pos.x && getBlock p.y == getBlock pos.y) b
+
+        blockValues =
+            List.map (\( _, val ) -> val) block
+    in
+    List.member v blockValues
